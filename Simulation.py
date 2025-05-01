942d760fbc17ea8a79e08f70e90843491d0acd25
Owen Exon <o.exon@outlook.com>
2025-05-01 17:41:02 +0100

    Added the frames folder to gitignore; added universe, (#IMAGE for exporting) and updated display

--- Simulation.py
+++ Simulation.py
@@ -1,4 +1,8 @@
 from MathPlus import *
+#IMAGE from PIL import Image as NewImage
+import time
+
+#IMAGE startTime = int(time.time())
 
 class graphicArrow():
     
@@ -25,6 +29,12 @@ class graphicArrow():
 
 
 
+def generateDisplay(position:Vector2D,radius:float|int,lineColour,fillColour="none") -> Circle:
+    tempCircle = Circle(position.point(),radius)
+    tempCircle.setOutline(lineColour)
+    if fillColour != "none": tempCircle.setFill(fillColour)
+    return tempCircle
+
 class pointParticle:
     
     """A particle with size of 0, a mass, and movement vectors, it is affected by force regions"""
@@ -33,14 +43,14 @@ class pointParticle:
         self.mass = mass
         self.position = position
         self.velocity = initialVelocity
-        self.displayPoint = Circle(position.point(),0.1)
+        self.display = generateDisplay(position,0.1,color_rgb(0,0,0),color_rgb(0,0,0))
         
     def tick(self,tickTime:float, resultantForce:Vector2D):
         acceleration = resultantForce / self.mass
         self.velocity += tickTime * acceleration
         dPosition = tickTime * self.velocity
         self.position += dPosition
-        self.displayPoint.move(dPosition.x,dPosition.y)
+        self.display.move(dPosition.x,dPosition.y)
 
 class singleDirectionForceRegion:
     
@@ -50,6 +60,36 @@ class singleDirectionForceRegion:
         self.radius = radius
         self.position = position
         self.force = force
+        self.display = generateDisplay(position,radius,color_rgb(255,0,0))
         
-def inForceRegion(object,forceRegion) -> bool:
+def isInForceRegion(object,forceRegion) -> bool:
     return distanceBetween2Vector2D(object.position,forceRegion.position) <= forceRegion.radius
+
+class universe:
+    
+    def __init__(self,name:str,resolution:list,coordLimits:list,gravity:Vector2D):
+        self.graphicsWindow = GraphWin(name,*resolution)
+        self.graphicsWindow.setCoords(*coordLimits)
+        self.gravity = gravity
+        self.actors = []
+        self.frame = 0
+    
+    def addObjects(self,*actors):
+        for actor in actors:
+            self.actors.append(actor)
+            actor.display.draw(self.graphicsWindow)
+            
+    def tick(self,tickTime):
+        self.frame += 1
+        for actor in self.actors:
+            if isinstance(actor,pointParticle):
+                resultantForce = self.gravity*actor.mass
+                for effector in self.actors:
+                    if isinstance(effector,singleDirectionForceRegion) and isInForceRegion(actor,effector):
+                        resultantForce += effector.force
+                actor.tick(tickTime,resultantForce)
+        #IMAGE if self.frame % 3 == 0:
+        #IMAGE     self.graphicsWindow.postscript(file="frames/tempImage.eps", colormode='color')
+        #IMAGE     img = NewImage.open("frames/tempImage.eps")
+        #IMAGE     img.save(f"frames/Time{startTime}Sim{self.frame}.bmp", "bmp")
+        time.sleep(tickTime)
