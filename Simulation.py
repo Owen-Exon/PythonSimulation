42e128fcb70b8f72690f2380b5def02f55da2567
Owen Exon <o.exon@outlook.com>
2025-05-09 20:35:38 +0100

    Merged point particle and force regions into single physics object ; now shuffles object order to add randomness ;

--- Simulation.py
+++ Simulation.py
@@ -1,17 +1,17 @@
 from MathPlus import *
 #IMAGE from PIL import Image as NewImage
 import time
-
+from random import shuffle
 #IMAGE startTime = int(time.time())
 
 class graphicArrow():
     
     def __init__(self, position:Vector2D,direction:Vector2D):
-        arrowVector = 0.25*direction
-        endPoint = position+arrowVector
+        endPoint = position+direction
+        argument = direction.arg()
         arrowBody = Line(position.point(),endPoint.point())
-        arrowL = Line(endPoint.point(),(endPoint + Vector2D(Angle(160,"Deg")+arrowVector.arg(),0.5)).point())
-        arrowR = Line(endPoint.point(),(endPoint + Vector2D(Angle(-160,"Deg")+arrowVector.arg(),0.5)).point())
+        arrowL = Line(endPoint.point(),(endPoint + Vector2D(Angle(160,"Deg")+argument,0.5)).point())
+        arrowR = Line(endPoint.point(),(endPoint + Vector2D(Angle(-160,"Deg")+argument,0.5)).point())
         
         self.arrowBody = arrowBody
         self.arrowL = arrowL
@@ -27,79 +27,51 @@ class graphicArrow():
         self.arrowL.undraw()
         self.arrowR.undraw()
 
-
-
-def generateDisplay(position:Vector2D,radius:float|int,lineColour,fillColour="none") -> Circle:
+def generateDisplay(position:Vector2D,radius:float|int,configs:dict):
     tempCircle = Circle(position.point(),radius)
-    tempCircle.setOutline(lineColour)
-    if fillColour != "none": tempCircle.setFill(fillColour)
+    for item in configs:
+        match item:
+            case "Fill":
+                tempCircle.setFill(configs[item])
+            case "Line":
+                tempCircle.setOutline(configs[item])
+            case "Width":
+                tempCircle.setWidth(configs[item])
     return tempCircle
 
-class pointParticle:
-    
-    """A particle with size of 0, a mass, and movement vectors, it is affected by force regions"""
-    # Add Colour and Display to init
-    def __init__(self, mass:float|int, position:Vector2D, initialVelocity:Vector2D,displayColour,displayRadius:float|int):
-        self.mass = mass
+class PhysicsObject:
+    def __init__(self,position:Vector2D, radius:float|int, display:dict):
         self.position = position
+        self.isMovable = False
+        self.hasForce = False
+        self.radius = radius
+        self.display = generateDisplay(position, radius, display)
+    
+    def setParticle(self, mass:int|float, initialVelocity:Vector2D):
+        self.isMovable = True
+        self.mass = mass
         self.velocity = initialVelocity
-        self.display = generateDisplay(position,displayRadius,displayColour,displayColour)
         
+    def setRadialForce(self, strength:float|int, atDistance:float|int, forceType:str="Acceleration"):
+        self.hasForce = True
+        self.forceType = f"radial:{forceType}"
+        self.strengthConstant = strength*atDistance**2
+    
+    def setSingleDirectionForce(self, radius:float|int, effect:Vector2D, forceType:str="Force"):
+        self.hasForce = True
+        self.forceRadius = radius
+        self.effect = effect
+        self.forceType = f"singleDirection:{forceType}"
+    
     def tick(self,tickTime:float, resultantForce:Vector2D=Vector2D(0,0), acceleration:Vector2D=Vector2D(0,0)):
         acceleration += resultantForce / self.mass
         self.velocity += tickTime * acceleration
         dPosition = tickTime * self.velocity
         self.position += dPosition
         self.display.move(dPosition.x,dPosition.y)
-
-class singleDirectionForceRegion:
-    
-    """This is a force region which applies a force in a single direction in a circle around a point"""
-    
-    def __init__(self, radius:float|int, position:Vector2D, effect:Vector2D, type:str="Force"):
-        self.radius = radius
-        self.position = position
-        self.effect = effect
-        self.type = type
-        self.display = generateDisplay(position,radius,color_rgb(255,0,0))
-
-class radialForceRegion:
-    
-    """This is a force region which applies a force around its centre"""
-    
-    def __init__(self, position:Vector2D, strength:float|int, atDistance:float|int, displayRadius,displayColour, type:str="Acceleration"):
-        
-        """Inits a radial force region
-        
-        Args:
-            radius (float|int): radius of the force region
-            position (Vector2D): Position of the force region
-            strength
-        """
-        
-        self.position = position
-        self.type = type
-        self.strengthConstant = strength*atDistance**2
-        self.display = generateDisplay(position,displayRadius,displayColour,displayColour)
     
 def isInForceRegion(object,forceRegion) -> bool:
-    return distanceBetween2Vector2D(object.position,forceRegion.position) <= forceRegion.radius
-
-class radialForcePointParticle:
-    def __init__(self, mass:float|int, position:Vector2D, initialVelocity:Vector2D, strength:float|int, atDistance:float|int,displayColour,displayRadius:float|int,type:str="Acceleration"):
-        self.mass = mass
-        self.position = position
-        self.velocity = initialVelocity
-        self.display = generateDisplay(position,displayRadius,displayColour,displayColour)
-        self.type = type
-        self.strengthConstant = strength*atDistance**2
-        
-    def tick(self,tickTime:float, resultantForce:Vector2D=Vector2D(0,0), acceleration:Vector2D=Vector2D(0,0)):
-        acceleration += resultantForce / self.mass
-        self.velocity += tickTime * acceleration
-        dPosition = tickTime * self.velocity
-        self.position += dPosition
-        self.display.move(dPosition.x,dPosition.y)
+    return distanceBetween2Vector2D(object.position,forceRegion.position) <= forceRegion.forceRadius
 
 class universe:
     
@@ -117,28 +89,28 @@ class universe:
             
     def tick(self,tickTime,sleepTime):
         self.frame += 1
+        shuffle(self.actors)
         for actor in self.actors:
-            if isinstance(actor,pointParticle|radialForcePointParticle):
+            if isinstance(actor , PhysicsObject) and actor.isMovable:
                 resultantAcceleration = self.gravity
                 resultantForce = Vector2D(0,0)
                 for effector in self.actors:
-                    if actor == effector: continue
-                    if isinstance(effector,singleDirectionForceRegion) and isInForceRegion(actor,effector):
-                        match effector.type:
-                            case "Force":
-                                resultantForce += effector.effect
-                            case "Acceleration":
-                                resultantAcceleration += effector.effect
-                    elif isinstance(effector, radialForceRegion|radialForcePointParticle):
-                        distance = distanceBetween2Vector2D(actor.position,effector.position)
-                        direction = (effector.position - actor.position).unitVector()
-                        effect = direction * (effector.strengthConstant/(distance**2))
-                        match effector.type:
+                    if actor == effector:continue
+                    if isinstance(effector,PhysicsObject) and effector.hasForce:
+                        effect = Vector2D(0,0)
+                        match effector.forceType.split(":")[0]:
+                            case "radial":
+                                distance = distanceBetween2Vector2D(actor.position,effector.position)
+                                direction = (effector.position - actor.position).unitVector()
+                                effect = direction * (effector.strengthConstant/(distance**2))
+                            case "singleDirection":
+                                if isInForceRegion(actor,effector): effect = effector.effect
+                        match effector.forceType.split(":")[1]:
                             case "Force":
                                 resultantForce += effect
                             case "Acceleration":
                                 resultantAcceleration += effect
-                            
+
                 actor.tick(tickTime,resultantForce,resultantAcceleration)
         #IMAGE if self.frame % 3 == 0:
         #IMAGE     self.graphicsWindow.postscript(file="frames/tempImage.eps", colormode='color')
