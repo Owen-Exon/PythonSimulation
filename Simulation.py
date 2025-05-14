d1d86024db32b72bc75a88940c5145dbe09656ea
Owen Exon <o.exon@outlook.com>
2025-05-14 12:25:15 +0100

    Added example to Simulation.py

--- Simulation.py
+++ Simulation.py
@@ -190,3 +190,13 @@ class universe:
                 self.timeMultiplier = self.otherTimeMultiplier
                 self.otherTimeMultiplier = tempTime
         self.graphicsWindow.close()
+        
+if __name__ == "__main__":
+    World = universe("World",[1000,1000],[-10,-10,10,10],Vector2D(0,0),1,0.5)
+
+    p1 = PhysicsObject(Vector2D(-5,0),.25)
+    p1.setParticle(1,Vector2D(5,0))
+    p2 = PhysicsObject(Vector2D(5,5),.5)
+    p2.setParticle(4,Vector2D(-5,-5))
+    
+    World.addObjects(p1,p2)
